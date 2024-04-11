71d57d6f4a38395d09105396367eb39522f5247f
blanusa <103044122+blanusa@users.noreply.github.com>
2024-04-11 20:15:07 +0200

    Added Service Rankings model

--- demo/src/main/java/com/example/demo/Model/ServiceUrgencyRankings.java
+++ demo/src/main/java/com/example/demo/Model/ServiceUrgencyRankings.java
@@ -1,10 +1,67 @@
 package com.example.demo.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="ServiceUrgencyRankings")
 public class ServiceUrgencyRankings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne
+    private Bus bus;
+
+    @Column
+    private float score;
+
+    @Column
+    private Integer rank;
+
+    public ServiceUrgencyRankings() {}
+
+    public ServiceUrgencyRankings(Bus bus, float score, Integer rank) {
+        this.bus = bus;
+        this.score = score;
+        this.rank = rank;
+    }
+
+    public ServiceUrgencyRankings(Bus bus) {
+        this.bus = bus;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public void setBus(Bus bus) {
+        this.bus = bus;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+    
 }
