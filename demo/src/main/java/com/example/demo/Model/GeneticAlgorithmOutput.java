95eaa5183692a0026155f45be5a77866946ecdb5
blanusa <103044122+blanusa@users.noreply.github.com>
2024-06-07 03:09:12 +0200

    Displaying on front works

--- demo/src/main/java/com/example/demo/Model/GeneticAlgorithmOutput.java
+++ demo/src/main/java/com/example/demo/Model/GeneticAlgorithmOutput.java
@@ -59,11 +59,11 @@ public class GeneticAlgorithmOutput {
         this.elBus = bus;
     }
 
-    public ChargingStation getChargingStationId() {
+    public ChargingStation getChStation() {
         return chStation;
     }
 
-    public void setChargingStationId(ChargingStation chStation) {
+    public void setChargingStation(ChargingStation chStation) {
         this.chStation = chStation;
     }
 
